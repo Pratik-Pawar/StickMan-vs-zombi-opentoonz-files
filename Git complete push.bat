ce6c91dd416d4eb6cac2896a20f34ef7ac0c4b4d
HP <Pratik@gmail.com>
2021-02-11 16:28:17 +0530

    batch msg added

--- Git complete push.bat
+++ Git complete push.bat
@@ -1,10 +1,16 @@
 @echo off
 git add .
+echo .
+echo "Stage file completed"
 echo.
 set /p mes="Enter commit Message: "
 echo.
 git commit -m "%mes%"
+echo .
+echo commit completed"
 echo.
 git push origin main
+echo .
+echo "push completed"
 echo.
 pause
