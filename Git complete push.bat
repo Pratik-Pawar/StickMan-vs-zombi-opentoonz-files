f0f44bfa28d7ff7f0b83eecbfc636dc101adb374
Lenovo <Pratik@Lenovo>
2021-02-11 16:23:29 +0530

    new line

--- Git complete push.bat
+++ Git complete push.bat
@@ -1,6 +1,10 @@
 @echo off
 git add .
+echo.
 set /p mes="Enter commit Message: "
+echo.
 git commit -m "%mes%"
+echo.
 git push origin main
+echo.
 pause
--- Git pull.bat
+++ Git pull.bat
@@ -1,2 +1,3 @@
+@echo off
 git pull https://github.com/Pratik-Pawar/StickMan-vs-zombi-opentoonz-files.git
 pause
