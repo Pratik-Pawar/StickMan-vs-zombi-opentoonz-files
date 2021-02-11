5295892f8fb1338a4b60d8af2ba2a90df31eeca2
Lenovo <Pratik@Lenovo>
2021-02-11 16:46:29 +0530

    improve batch file feedback 2

--- Git complete push.bat
+++ Git complete push.bat
@@ -2,6 +2,7 @@
 
 echo Staging file: & echo.
 git add .
+echo.
 echo ********* Staging Completed ********* & echo.
 
 echo Commiting: 
@@ -9,10 +10,12 @@ echo.
 set /p mes="Enter commit Message: "
 echo.
 git commit -m "%mes%"
+echo.
 echo ********* Commit  Completed ********* & echo.
 
 echo Pushing to remot: & echo.
 git push origin main
+echo.
 echo ********* Pushing Completed ********* & echo.
 
 pause
